bb9ac71892da2ae82d0f4302a3221248d6a87b8b
Marcelo Beghe <marcelo.beghe@gmail.com>
2020-04-20 00:44:07 -0300

    react-player installed and integrated to detail view.

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
+import ReactPlayer from 'react-player';
 
 export default function VideoDetails() {
     return(
@@ -11,8 +12,10 @@ export default function VideoDetails() {
             justify="center"
             style={{ minHeight: '100vh' }}
         >
-            <Grid item xs={3}>
-                Test
+            <Grid item>
+                <Paper>
+                    <ReactPlayer url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" controls={true}/>
+                </Paper>
             </Grid>   
         </Grid> 
     );
